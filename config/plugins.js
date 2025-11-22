ab91d496ae74af1a8d47ab24dc3e0fdc410660a5
Utitbest <utitbesta@gmail.com>
2025-11-22 23:11:52 +0100

    returned

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,1 @@
-module.exports = ({ env }) => ({
-  'transfer': {
-    enabled: true,
-    config: {
-      token: {
-        salt: env('TRANSFER_TOKEN_SALT'),
-      },
-    },
-  },
-});
+module.exports = () => ({});
